a17defabc62bef7a1ecd294d5035f81f6ae18761
venkatalolla <suryasaicharan93@gmail.com>
2018-09-25 23:19:10 -0400

    test

--- src/test/java/seleniumtest/tasks/AppTest.java
+++ src/test/java/seleniumtest/tasks/AppTest.java
@@ -14,7 +14,7 @@ public class AppTest {
 
 	@BeforeClass
 	public void testSetUp() {
-		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/cache/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/cache/chromedriver/chromedriver");
 		driver = new ChromeDriver();
 	}
 
