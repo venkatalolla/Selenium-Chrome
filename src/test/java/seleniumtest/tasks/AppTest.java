cd5a712bb912f470ca8dcffb9f09739e609ecc2b
venkatalolla <suryasaicharan93@gmail.com>
2018-09-25 22:46:36 -0400

    Initial commit

---
+++ src/test/java/seleniumtest/tasks/AppTest.java
@@ -0,0 +1,33 @@
+package seleniumtest.tasks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	private WebDriver driver; 
+	String appURL = "http://tasks-tasks-dev.oseapps.levvel-labs.io/";
+
+	@BeforeClass
+	public void testSetUp() {
+		System.setProperty("webdriver.chrome.driver", "/Users/suryalolla/Downloads/selenium-java-3.14.0/chromedriver");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void verifyOpenShiftTasksPageTittle() {
+		driver.navigate().to(appURL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "OpenShift Demo Tasks");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
